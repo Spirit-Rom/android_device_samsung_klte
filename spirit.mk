314492c4bd1e9ad81c84291c9c8a9db81b7ecab0
fumes007 <fumesbond@gmail.com>
2015-02-28 06:44:32 -0500

    Spirit stuff

---
+++ spirit.mk
@@ -0,0 +1,8 @@
+$(call inherit-product, vendor/spirit/config/common_phone.mk)
+
+$(call inherit-product, vendor/spirit/config/nfc_enhanced.mk)
+
+$(call inherit-product, device/samsung/klte/full_klte.mk)
+
+PRODUCT_DEVICE := klte
+PRODUCT_NAME := spirit_klte
